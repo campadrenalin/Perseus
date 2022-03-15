33c33e7d81bcf1a19e3aad3f01e5d806d963b12d
Philip Horger <campadrenalin@gmail.com>
2022-03-15 16:03:51 -0700

    Initial playable anything

---
+++ perseus/src/main.rs
@@ -0,0 +1,79 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::rapier::na::Vector2;
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Player Movement Example".to_string(),
+            width: 1000.0,
+            height: 1000.0,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_player.system())
+        .add_system(player_movement.system())
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .run();
+}
+
+#[derive(Component)]
+struct Player(f32);
+
+fn spawn_player(mut commands: Commands, mut rapier_config: ResMut<RapierConfiguration>) {
+    // Set gravity to 0 and spawn camera.
+    rapier_config.gravity = Vector2::zeros();
+    commands
+        .spawn()
+        .insert_bundle(OrthographicCameraBundle::new_2d());
+
+    let sprite_size_x = 40.0;
+    let sprite_size_y = 40.0;
+
+    rapier_config.scale = 20.0;
+    let collider_size_x = sprite_size_x / rapier_config.scale;
+    let collider_size_y = sprite_size_y / rapier_config.scale;
+
+    // Spawn player
+    commands
+        .spawn()
+        .insert_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.0,0.0,0.0),
+                custom_size: Some(Vec2::new(sprite_size_x, sprite_size_y)),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert_bundle(RigidBodyBundle::default())
+        .insert_bundle(ColliderBundle {
+            position: [collider_size_x/2.0, collider_size_y/2.0].into(),
+            ..Default::default()
+        })
+        .insert(ColliderPositionSync::Discrete)
+        .insert(ColliderDebugRender::with_id(0))
+        .insert(Player(300.0));
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    rapier_parameters: Res<RapierConfiguration>,
+    mut player_info: Query<(&Player, &mut RigidBodyVelocityComponent)>,
+) {
+    for (player, mut rb_vels) in player_info.iter_mut() {
+        let up = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
+        let down = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
+        let left = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
+        let right = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
+        
+        let x_axis = -(left as i8) + right as i8;
+        let y_axis = -(down as i8) + up as i8;
+
+        let mut move_delta = Vector2::new(x_axis as f32, y_axis as f32);
+        if move_delta != Vector2::zeros() {
+            move_delta /= move_delta.magnitude() * rapier_parameters.scale;
+        }
+
+        rb_vels.linvel = move_delta * player.0;
+    }
+}
