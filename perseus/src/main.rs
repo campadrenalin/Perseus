02cc4c5e12f12408c7a78a7b86212500c9a13cff
Philip Horger <campadrenalin@gmail.com>
2022-03-17 11:55:22 -0700

    Multiple players

--- perseus/src/main.rs
+++ perseus/src/main.rs
@@ -15,8 +15,8 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera.system())
-        .add_startup_system(spawn_player.system())
-        .add_system(player_movement.system())
+        .add_startup_system(spawn_players.system())
+        .add_system(keyboard_movement.system())
         .add_system(print_positions.system())
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .run();
@@ -44,14 +44,33 @@ fn spawn_camera(mut commands: Commands, mut rapier_config: ResMut<RapierConfigur
 }
 
 #[derive(Component)]
-struct Player(f32);
+struct MoveSpeed(f32);
 
-fn spawn_player(mut commands: Commands, rapier_config: ResMut<RapierConfiguration>) {
+#[derive(Component)]
+struct KeyboardMoveable {
+    up: bevy::input::keyboard::KeyCode,
+    down: bevy::input::keyboard::KeyCode,
+}
+
+fn spawn_players(mut commands: Commands) {
+    let spacing = 20.0;
+    spawn_player(&mut commands, -spacing, KeyboardMoveable {
+        up: bevy::input::keyboard::KeyCode::W,
+        down: bevy::input::keyboard::KeyCode::S,
+    });
+    spawn_player(&mut commands, spacing, KeyboardMoveable {
+        up: bevy::input::keyboard::KeyCode::Up,
+        down: bevy::input::keyboard::KeyCode::Down,
+    });
+}
+
+fn spawn_player(
+    commands: &mut Commands,
+    x: f32,
+    motion: KeyboardMoveable
+) {
     let sprite_size_x = 40.0;
-    let sprite_size_y = 40.0;
-
-    let collider_size_x = sprite_size_x / rapier_config.scale;
-    let collider_size_y = sprite_size_y / rapier_config.scale;
+    let sprite_size_y = 400.0;
 
     // Spawn player
     commands
@@ -66,27 +85,29 @@ fn spawn_player(mut commands: Commands, rapier_config: ResMut<RapierConfiguratio
         })
         .insert_bundle(RigidBodyBundle::default())
         .insert_bundle(ColliderBundle {
-            position: [collider_size_x/2.0, collider_size_y/2.0].into(),
+            position: [x, 0.0].into(),
             ..Default::default()
         })
         .insert(ColliderPositionSync::Discrete)
         .insert(ColliderDebugRender::with_id(0))
-        .insert(Player(300.0))
+        .insert(MoveSpeed(300.0))
+        .insert(motion)
         .insert(Tag(EntityPrimaryTag::Player));
 }
 
-fn player_movement(
+fn keyboard_movement(
     keyboard_input: Res<Input<KeyCode>>,
     rapier_parameters: Res<RapierConfiguration>,
-    mut player_info: Query<(&Player, &mut RigidBodyVelocityComponent)>,
+    mut player_info: Query<(
+        &MoveSpeed,
+        &KeyboardMoveable,
+        &mut RigidBodyVelocityComponent)>,
 ) {
-    for (player, mut rb_vels) in player_info.iter_mut() {
-        let up = keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up);
-        let down = keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down);
-        let left = keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left);
-        let right = keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right);
-        
-        let x_axis = -(left as i8) + right as i8;
+    for (speed, keys, mut rb_vels) in player_info.iter_mut() {
+        let up = keyboard_input.pressed(keys.up);
+        let down = keyboard_input.pressed(keys.down);
+
+        let x_axis = 0 as i8;
         let y_axis = -(down as i8) + up as i8;
 
         let mut move_delta = vec2(x_axis as f32, y_axis as f32);
@@ -94,7 +115,7 @@ fn player_movement(
             move_delta /= move_delta.magnitude() * rapier_parameters.scale;
         }
 
-        rb_vels.linvel = move_delta * player.0;
+        rb_vels.linvel = move_delta * speed.0;
     }
 }
 
